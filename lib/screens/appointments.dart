40c48eafc2e0a685a82fcf72a50e197aaa6f4f46
bobgolosinda <sed403483@gmail.com>
2024-11-16 05:58:14 +0800

    Can now give feedback to 'done' appointments'.

--- lib/screens/appointments.dart
+++ lib/screens/appointments.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:pamfurred/components/screen_transitions.dart';
 import 'package:pamfurred/components/time_and_date_formatter.dart';
 import 'package:pamfurred/components/title_text.dart';
 import 'package:pamfurred/providers/appointments_provider.dart';
@@ -39,7 +38,6 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
 
   @override
   Widget build(BuildContext context) {
-    // Watch the appointment details provider for appointment data
     final appointmentAsyncValue = ref.watch(appointmentDetailsProvider);
 
     return Scaffold(
@@ -92,16 +90,14 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
 
   Widget _buildAppointmentList(
       int tabIndex, List<Map<String, dynamic>> appointmentList) {
-    // Filter appointments based on the selected tab index
     final filteredAppointments = appointmentList.where((appointment) {
-      final dateFormat = DateFormat('MM/dd/yyyy'); // Define the date format
+      final dateFormat = DateFormat('MM/dd/yyyy');
       DateTime appointmentDate;
 
       try {
         appointmentDate = dateFormat.parse(appointment['appointment_date']);
       } catch (e) {
-        appointmentDate =
-            DateTime.now(); // Fallback to the current date if parsing fails
+        appointmentDate = DateTime.now();
       }
 
       switch (tabIndex) {
@@ -123,12 +119,10 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
       }
     }).toList();
 
-    // If no filtered appointments match, show a message
     if (filteredAppointments.isEmpty) {
       return const Center(child: Text('No Appointments Available'));
     }
 
-    // Return a list of filtered appointments
     return ListView.builder(
       physics: const BouncingScrollPhysics(),
       itemCount: filteredAppointments.length,
@@ -142,13 +136,20 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
             GestureDetector(
               onTap: appointment['appointment_status'] == 'Done'
                   ? () {
-                      Navigator.push(
-                          context, slideUpRoute(const GiveFeedbackScreen()));
+                      ref.read(tappedSpAppointmentIdProvider.notifier).state =
+                          appointment['sp_id'];
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        builder: (BuildContext context) {
+                          return const GiveFeedbackBottomSheet();
+                        },
+                      );
                     }
                   : null,
               child: ListTile(
-                title: customBoldWeightRegularText(context,
-                    '${appointment['establishment_name'] ?? 'N/A'}'), // Fallback if null
+                title: customBoldWeightRegularText(
+                    context, '${appointment['establishment_name'] ?? 'N/A'}'),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
@@ -156,16 +157,14 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
                     Text(
                       appointment['appointment_date'] == null
                           ? 'N/A'
-                          : formatDate(appointment[
-                              'appointment_date']), // Fallback if null
+                          : formatDate(appointment['appointment_date']),
                       style: const TextStyle(color: darkGreyColor),
                     ),
                     const SizedBox(height: primarySizedBox),
                     Text(
                       appointment['appointment_time'] == null
                           ? 'N/A'
-                          : formatTime(appointment[
-                              'appointment_time']), // Fallback if null
+                          : formatTime(appointment['appointment_time']),
                       style: const TextStyle(color: greyColor),
                     ),
                   ],
@@ -178,8 +177,7 @@ class AppointmentsScreenState extends ConsumerState<AppointmentsScreen>
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   Text(
-                    appointment['appointment_status'] ??
-                        'Unknown', // Fallback if null
+                    appointment['appointment_status'] ?? 'Unknown',
                     style: TextStyle(
                       color: statusColors[appointment['appointment_status']] ??
                           Colors.black87,
--- lib/screens/give_feedback.dart
+++ lib/screens/give_feedback.dart
@@ -1,98 +1,151 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pamfurred/components/custom_appbar.dart';
+import 'package:intl/intl.dart';
+import 'package:pamfurred/components/custom_padded_button.dart';
+import 'package:pamfurred/components/globals.dart';
+import 'package:pamfurred/providers/appointments_provider.dart';
+import 'package:pamfurred/providers/user_id.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class GiveFeedbackScreen extends ConsumerStatefulWidget {
-  const GiveFeedbackScreen({super.key});
+class GiveFeedbackBottomSheet extends ConsumerStatefulWidget {
+  const GiveFeedbackBottomSheet({super.key});
 
   @override
-  ConsumerState<GiveFeedbackScreen> createState() => GiveFeedbackScreenState();
+  GiveFeedbackBottomSheetState createState() => GiveFeedbackBottomSheetState();
 }
 
-class GiveFeedbackScreenState extends ConsumerState<GiveFeedbackScreen> {
+class GiveFeedbackBottomSheetState
+    extends ConsumerState<GiveFeedbackBottomSheet> {
   int _rating = 0;
   final TextEditingController _reviewController = TextEditingController();
-
-  void _submitFeedback() {
-    final String review = _reviewController.text;
-
-    if (_rating == 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please provide a star rating.')),
-      );
-      return;
-    }
-
-    // You can handle the feedback submission here.
-    print('Rating: $_rating');
-    print('Review: $review');
-
-    // Clear fields after submission
-    setState(() {
-      _rating = 0;
-      _reviewController.clear();
-    });
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Feedback submitted!')),
-    );
-  }
-
-  Widget _buildStar(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _rating = index;
-        });
-      },
-      child: Icon(
-        index <= _rating ? Icons.star : Icons.star_border,
-        color: Colors.amber,
-        size: 40.0,
-      ),
-    );
-  }
+  String? _errorMessage;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBarWithTitle(context, 'Give Feedback'),
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+    final spDetails = ref.watch(appointmentSpIndexProvider);
+    final spName = spDetails?['establishment_name'];
+
+    return Padding(
+      padding: const EdgeInsets.all(tertiarySizedBox),
+      child: Container(
+        padding: const EdgeInsets.only(top: secondarySizedBox),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(tertiaryBorderRadius),
+              topRight: Radius.circular(tertiaryBorderRadius)),
+        ),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Rate us',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            Center(
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(tertiaryBorderRadius),
+                  ),
+                  color: greyColor,
+                ),
+                width: 50,
+                height: 5,
+              ),
             ),
-            const SizedBox(height: 8.0),
+            const SizedBox(height: tertiarySizedBox),
+            Text(
+              'Rate $spName',
+              style: const TextStyle(fontSize: titleFont),
+            ),
+            const SizedBox(height: secondarySizedBox),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(5, (index) => _buildStar(index + 1)),
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(5, (index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _rating = index + 1;
+                      _errorMessage = null;
+                    });
+                  },
+                  child: Icon(
+                    index < _rating ? Icons.star : Icons.star_border,
+                    color: secondaryColor,
+                    size: 40.0,
+                    weight: .15,
+                  ),
+                );
+              }),
             ),
-            const SizedBox(height: 24.0),
+            const SizedBox(height: tertiarySizedBox),
             const Text(
               'Write a review',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: titleFont),
             ),
-            const SizedBox(height: 8.0),
+            const SizedBox(height: secondarySizedBox),
             TextField(
               controller: _reviewController,
               maxLines: 4,
               decoration: const InputDecoration(
                 hintText: 'Tell us about your experience',
-                border: OutlineInputBorder(),
+                hintStyle: TextStyle(fontSize: regularText, color: greyColor),
+                border: OutlineInputBorder(borderSide: BorderSide(width: .25)),
               ),
             ),
-            const SizedBox(height: 24.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: _submitFeedback,
-                child: const Text('Submit Feedback'),
+            if (_errorMessage != null) ...[
+              const SizedBox(height: 8.0),
+              Text(
+                _errorMessage!,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
+            ],
+            const SizedBox(height: quaternarySizedBox),
+            Align(
+              alignment: Alignment.centerRight,
+              child: customPaddedTextButton(
+                text: 'Submit feedback',
+                onPressed: () async {
+                  if (_rating == 0) {
+                    setState(() {
+                      _errorMessage = 'Please provide a star rating.';
+                    });
+                    return;
+                  }
+
+                  try {
+                    final petOwnerId = ref.watch(userIdProvider);
+                    final spAppointmentId =
+                        ref.watch(tappedSpAppointmentIdProvider);
+
+                    DateTime dateNow = DateTime.now();
+                    String formattedDate =
+                        DateFormat('yyyy-MM-dd').format(dateNow);
+
+                    final supabase = Supabase.instance.client;
+
+                    final response = await supabase.from('feedback').insert({
+                      'pet_owner_id': petOwnerId,
+                      'sp_id': spAppointmentId,
+                      'review': _reviewController.text,
+                      'review_date': formattedDate,
+                      'compound_score': null,
+                      'rating': _rating,
+                    });
+
+                    if (context.mounted) {
+                      Navigator.of(context).pop();
+                    }
+
+                    return response;
+                  } catch (e) {
+                    setState(() {
+                      _errorMessage =
+                          'Failed to submit feedback. Please try again.';
+                    });
+                    print('Error inserting feedback: $e');
+                  }
+                },
               ),
             ),
+            const SizedBox(height: tertiarySizedBox),
           ],
         ),
       ),

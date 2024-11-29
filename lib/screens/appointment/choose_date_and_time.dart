78b9a81ced8f282a31e65295b2917581c1b07ac2
bobgolosinda <sed403483@gmail.com>
2024-11-30 05:49:25 +0800

    Added a color legend for selecting appointment date and time.

--- lib/screens/appointment/choose_date_and_time.dart
+++ lib/screens/appointment/choose_date_and_time.dart
@@ -19,7 +19,7 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
     final selectedTimeslot = ref.watch(selectedTimeslotProvider);
 
     return Scaffold(
-      appBar: customAppBarWithTitleAndWidget(context, 'Choose Date & Time', [
+      appBar: customAppBarWithTitleAndWidget(context, 'Select Date & Time', [
         TextButton(
           style: ButtonStyle(
               shape: WidgetStateProperty.all<RoundedRectangleBorder>(
@@ -54,7 +54,17 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Date Picker Section
-            const Text('Select a Date:', style: TextStyle(fontSize: titleFont)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Select a date:',
+                    style: TextStyle(fontSize: titleFont)),
+                IconButton(
+                  icon: const Icon(Icons.help_outline_outlined, size: 25),
+                  onPressed: () => _showColorLegend(context),
+                ),
+              ],
+            ),
             const SizedBox(height: 8),
             TableCalendar(
               focusedDay: selectedDate != null
@@ -204,11 +214,10 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
               ),
             ),
 
-            const SizedBox(height: 24),
+            const SizedBox(height: quaternarySizedBox),
 
             // Timeslot Selector Section
-            const Text('Available Timeslots:',
-                style: TextStyle(fontSize: titleFont)),
+            const Text('Select time:', style: TextStyle(fontSize: titleFont)),
             const SizedBox(height: 8),
             if (selectedDate == null)
               const Text('Please select a date to see available timeslots.')
@@ -259,6 +268,95 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget getLegendWidget(Color color, String text) {
+    return Row(
+      children: [
+        Container(
+          width: 15,
+          height: 15,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: color, // Accepts any Color now
+          ),
+        ),
+        const SizedBox(width: secondarySizedBox),
+        Text(
+          text,
+          style: const TextStyle(fontSize: smallText),
+        ),
+      ],
+    );
+  }
+
+  void _showColorLegend(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white, // White background for a clean look
+          shape: RoundedRectangleBorder(
+            borderRadius:
+                BorderRadius.circular(16), // Rounded corners for elegance
+          ),
+          elevation: 8, // Adding some elevation to create a floating effect
+          title: const Text(
+            'Color Legend',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black, // Dark title for contrast
+            ),
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                getLegendWidget(Colors.green, 'Available'),
+                const SizedBox(height: 12), // Increased space between items
+                getLegendWidget(Colors.grey, 'Not available'),
+                const SizedBox(height: 12),
+                getLegendWidget(Colors.red, 'Fully booked'),
+                const SizedBox(height: 12),
+                getLegendWidget(primaryColor, 'Date today'),
+                const SizedBox(height: 12),
+                getLegendWidget(secondaryColor, 'Date selected'),
+              ],
+            ),
+          ),
+          actions: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(bottom: 8.0), // Padding for spacing
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.blue, // Correct property to set background color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded button
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 10), // Button padding
+                ),
+                child: const Text(
+                  'OK',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }

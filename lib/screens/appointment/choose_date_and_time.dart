f4da1b864438a44640c3d770796a26688f785fab
bobgolosinda <sed403483@gmail.com>
2024-11-29 20:01:24 +0800

    Display the date in appointment booking by default and not a popup.

--- lib/screens/appointment/choose_date_and_time.dart
+++ lib/screens/appointment/choose_date_and_time.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:pamfurred/components/custom_appbar.dart';
-import 'package:pamfurred/components/custom_padded_button.dart';
 import 'package:pamfurred/components/globals.dart';
 import 'package:pamfurred/components/screen_transitions.dart';
 import 'package:pamfurred/components/time_and_date_formatter.dart';
 import 'package:pamfurred/providers/available_timeslots_provider.dart';
 import 'package:pamfurred/providers/serviceprovider_provider.dart';
 import 'package:pamfurred/screens/appointment/appointment_summary.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ChooseDateAndTimeScreen extends ConsumerWidget {
   const ChooseDateAndTimeScreen({super.key});
@@ -56,32 +56,51 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
             // Date Picker Section
             const Text('Select a Date:', style: TextStyle(fontSize: titleFont)),
             const SizedBox(height: 8),
-            Text(
-              selectedDate != null
-                  ? DateFormat('MMMM dd, yyyy')
-                      .format(DateTime.parse(selectedDate))
-                  : 'No date selected',
-              style: const TextStyle(fontSize: regularText),
-            ),
-            const SizedBox(height: 16),
-            customPaddedTextButtonWIthSecondaryColor(
-              onPressed: () async {
-                final pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: selectedDate != null
-                      ? DateTime.parse(selectedDate)
-                      : DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime.now().add(const Duration(days: 30)),
-                );
-                if (pickedDate != null) {
-                  ref.read(selectedDateProvider.notifier).state =
-                      DateFormat('yyyy-MM-dd').format(pickedDate);
-                  ref.read(selectedTimeslotProvider.notifier).state =
-                      null; // Clear timeslot when date changes
-                }
+            TableCalendar(
+              focusedDay: selectedDate != null
+                  ? DateTime.parse(selectedDate)
+                  : DateTime.now(),
+              firstDay: DateTime.now(),
+              lastDay: DateTime.now().add(const Duration(days: 30)),
+              selectedDayPredicate: (day) {
+                return selectedDate != null &&
+                    DateFormat('yyyy-MM-dd').format(day) == selectedDate;
               },
-              text: "Choose Date",
+              onDaySelected: (selectedDay, focusedDay) {
+                ref.read(selectedDateProvider.notifier).state =
+                    DateFormat('yyyy-MM-dd').format(selectedDay);
+                ref.read(selectedTimeslotProvider.notifier).state =
+                    null; // Clear timeslot when date changes
+              },
+              // Styling of the day cells
+              calendarStyle: CalendarStyle(
+                todayDecoration: const BoxDecoration(
+                  color: primaryColor,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: const BoxDecoration(
+                  color: secondaryColor,
+                  shape: BoxShape.circle,
+                ),
+                defaultDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black.withOpacity(0.1),
+                  ),
+                ),
+              ),
+              headerStyle: const HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+                leftChevronIcon: Icon(
+                  Icons.arrow_left,
+                  color: Colors.black,
+                ),
+                rightChevronIcon: Icon(
+                  Icons.arrow_right,
+                  color: Colors.black,
+                ),
+              ),
             ),
             const SizedBox(height: 24),
 
@@ -93,50 +112,48 @@ class ChooseDateAndTimeScreen extends ConsumerWidget {
               const Text('Please select a date to see available timeslots.')
             else
               ref.watch(availableTimeslotsProvider(selectedDate)).when(
-                    data: (timeslotData) {
-                      final timeslotMap = <String, List<String>>{};
-                      for (var slot in timeslotData) {
-                        final date = slot['availability_date'];
-                        final timeslots = List<String>.from(slot['timeslots']);
-                        timeslotMap[date] = timeslots;
-                      }
+                  data: (timeslotData) {
+                    final timeslotMap = <String, List<String>>{};
+                    for (var slot in timeslotData) {
+                      final date = slot['availability_date'];
+                      final timeslots = List<String>.from(slot['timeslots']);
+                      timeslotMap[date] = timeslots;
+                    }
 
-                      if (timeslotMap.containsKey(selectedDate)) {
-                        final timeslots = timeslotMap[selectedDate]!;
-                        return Wrap(
-                          spacing: 8.0, // Space between chips
-                          runSpacing: 8.0, // Space between rows of chips
-                          children: timeslots.map((timeslot) {
-                            return ChoiceChip(
-                              label: Text(formatTime(timeslot)),
-                              selected: selectedTimeslot == timeslot,
-                              onSelected: (selected) {
-                                ref
-                                    .read(selectedTimeslotProvider.notifier)
-                                    .state = selected ? timeslot : null;
-                              },
-                              selectedColor: secondaryColor,
-                              checkmarkColor: lighterGreyColor,
-                              backgroundColor: Colors.transparent,
-                              labelStyle: TextStyle(
-                                  color: selectedTimeslot == timeslot
-                                      ? lighterGreyColor
-                                      : Colors.black,
-                                  fontSize: regularText),
-                            );
-                          }).toList(),
-                        );
-                      } else {
-                        return const Center(
-                            child:
-                                Text('No available timeslots for this date.'));
-                      }
-                    },
-                    loading: () =>
-                        const Center(child: CircularProgressIndicator()),
-                    error: (error, stackTrace) =>
-                        Center(child: Text('Error: $error')),
-                  ),
+                    if (timeslotMap.containsKey(selectedDate)) {
+                      final timeslots = timeslotMap[selectedDate]!;
+                      return Wrap(
+                        spacing: 8.0, // Space between chips
+                        runSpacing: 8.0, // Space between rows of chips
+                        children: timeslots.map((timeslot) {
+                          return ChoiceChip(
+                            label: Text(formatTime(timeslot)),
+                            selected: selectedTimeslot == timeslot,
+                            onSelected: (selected) {
+                              ref
+                                  .read(selectedTimeslotProvider.notifier)
+                                  .state = selected ? timeslot : null;
+                            },
+                            selectedColor: secondaryColor,
+                            checkmarkColor: lighterGreyColor,
+                            backgroundColor: Colors.transparent,
+                            labelStyle: TextStyle(
+                                color: selectedTimeslot == timeslot
+                                    ? lighterGreyColor
+                                    : Colors.black,
+                                fontSize: regularText),
+                          );
+                        }).toList(),
+                      );
+                    } else {
+                      return const Center(
+                          child: Text('No available timeslots for this date.'));
+                    }
+                  },
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                  error: (error, stackTrace) =>
+                      Center(child: Text('Error: $error'))),
           ],
         ),
       ),

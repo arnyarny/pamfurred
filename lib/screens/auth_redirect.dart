58143d1b8c28d90cc63b69f774be3d8c67f98f87
bobgolosinda <sed403483@gmail.com>
2024-12-10 15:22:50 +0800

    Merge remote-tracking branch 'origin/maintwo'

--- lib/screens/auth_redirect.dart
+++ lib/screens/auth_redirect.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:pamfurred/backend_logic_files/realtime_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import Riverpod package

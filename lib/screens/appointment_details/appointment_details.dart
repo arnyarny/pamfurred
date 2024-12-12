6a4d2d363b8c9d086ed2b90b7f451866525b78c6
bobgolosinda <sed403483@gmail.com>
2024-12-12 18:01:58 +0800

    Modified appointment details screen.

--- lib/screens/appointment_details/appointment_details.dart
+++ lib/screens/appointment_details/appointment_details.dart
@@ -1,118 +1,181 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pamfurred/components/custom_appbar.dart';
+import 'package:pamfurred/components/globals.dart';
+import 'package:pamfurred/components/regular_text.dart';
 import 'package:pamfurred/components/time_and_date_formatter.dart';
+import 'package:pamfurred/components/title_text.dart';
+import 'package:pamfurred/providers/appointment_services_and_packages.dart';
 import 'package:pamfurred/providers/appointments_provider.dart';
+import 'package:pamfurred/providers/global_providers.dart';
+import 'package:pamfurred/screens/appointment/appointment_components/components.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class AppointmentDetailsScreen extends StatelessWidget {
+class AppointmentDetailsScreen extends ConsumerStatefulWidget {
   const AppointmentDetailsScreen({super.key});
 
   @override
+  ConsumerState<AppointmentDetailsScreen> createState() =>
+      AppointmentDetailsScreenState();
+}
+
+class AppointmentDetailsScreenState
+    extends ConsumerState<AppointmentDetailsScreen> {
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBarWithTitle(context, 'Appointment Details'),
-      backgroundColor: Colors.white,
-      body: Consumer(
-        builder: (context, ref, child) {
-          final appointmentId = ref.watch(tappedSpAppointmentIdProvider);
+    final appointmentId = ref.watch(tappedSpAppointmentIdProvider);
+    if (appointmentId.isEmpty) {
+      return const Center(child: Text('No appointment selected.'));
+    }
 
-          if (appointmentId == '' || appointmentId.isEmpty) {
-            return const Center(child: Text('No appointment selected.'));
-          }
+    final specificAppointmentDetails =
+        ref.watch(specificAppointmentDetailsProvider(appointmentId));
+    final combinedDetails =
+        ref.watch(appointmentServicesAndPackagesProvider(appointmentId));
 
-          final specificAppointmentDetails =
-              ref.watch(specificAppointmentDetailsProvider(appointmentId));
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.white,
+          appBar: customAppBarWithTitle(context, 'Appointment Details'),
+          body: specificAppointmentDetails.when(
+            data: (details) {
+              return combinedDetails.when(
+                data: (combined) {
+                  final services =
+                      (combined['services'] as List<AppointmentService>?) ?? [];
+                  final packages =
+                      (combined['packages'] as List<AppointmentPackage>?) ?? [];
 
-          return specificAppointmentDetails.when(
-            data: (data) {
-              if (data == null) {
-                return const Center(child: Text('No details available.'));
-              }
+                  final appointmentDate =
+                      details?['appointment_date'].toString();
+                  final appointmentTime =
+                      details?['appointment_time'].toString();
 
-              final appointmentDate =
-                  data['appointment_date'] ?? 'Not available';
-
-              return SingleChildScrollView(
-                // Wrap the body in a SingleChildScrollView to avoid overflow
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildDetailCard('Appointment ID', appointmentId),
-                    const SizedBox(height: 12),
-                    _buildDetailCard('Appointment Date',
-                        secondaryFormatDate(appointmentDate)),
-                    const SizedBox(height: 12),
-                    _buildDetailCard('Appointment Time',
-                        formatTime(data['appointment_time'])),
-                    const SizedBox(height: 12),
-                    _buildDetailCard(
-                        'Service Provider', data['establishment_name']),
-                    const SizedBox(height: 12),
-                    _buildDetailCard('Pet Name', data['pet_name']),
-                  ],
-                ),
+                  return SingleChildScrollView(
+                    physics: BouncingScrollPhysics(),
+                    child: Center(
+                      child: SizedBox(
+                        width: screenPadding(context),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: tertiarySizedBox),
+                            getAppointmentTitle(context, 'Service provider'),
+                            const SizedBox(height: primarySizedBox),
+                            getAppointmentDetail(
+                                context, details?['establishment_name']),
+                            const SizedBox(height: tertiarySizedBox),
+                            getAppointmentTitle(context, 'Pet name'),
+                            const SizedBox(height: primarySizedBox),
+                            getAppointmentDetail(context, details?['pet_name']),
+                            const SizedBox(height: tertiarySizedBox),
+                            getAppointmentTitle(context, 'Appointment type'),
+                            const SizedBox(height: primarySizedBox),
+                            getAppointmentDetail(
+                                context, details?['appointment_type']),
+                            const SizedBox(height: tertiarySizedBox),
+                            getAppointmentTitle(context, 'Address'),
+                            const SizedBox(height: primarySizedBox),
+                            wrappedText(
+                                context, details?['appointment_address'] ?? ''),
+                            const SizedBox(height: tertiarySizedBox),
+                            getAppointmentTitle(context, 'Date and time'),
+                            const SizedBox(height: primarySizedBox),
+                            getAppointmentDetail(context,
+                                '${secondaryFormatDate(appointmentDate!)}, ${formatTime(appointmentTime!)}'),
+                            const SizedBox(height: tertiarySizedBox),
+                            if (services.isNotEmpty) ...[
+                              getAppointmentTitle(context, 'Services'),
+                              const SizedBox(height: primarySizedBox),
+                              ...services.map((service) => buildCartItem(
+                                  name: '${service.serviceName}',
+                                  price: '${service.servicePrice.toString()}')),
+                            ],
+                            const SizedBox(height: primarySizedBox),
+                            if (packages.isNotEmpty) ...[
+                              getAppointmentTitle(context, 'Packages'),
+                              const SizedBox(height: primarySizedBox),
+                              ...packages.map((package) => buildCartItem(
+                                  name: '${package.packageName}',
+                                  price: '${package.packagePrice.toString()}')),
+                            ],
+                            const SizedBox(height: secondarySizedBox),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                getAppointmentTotalTitle(context, 'Total'),
+                                regularPrimaryColoredBoldTextWidget(
+                                    '₱${details?['total_amount']}')
+                              ],
+                            ),
+                            const SizedBox(height: quaternarySizedBox),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (error, stackTrace) =>
+                    Center(child: Text('Error: $error')),
               );
             },
-            error: (error, stackTrace) {
-              return Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.error, color: Colors.red),
-                    const SizedBox(height: 8),
-                    Text('Error: $error'),
-                  ],
-                ),
-              );
-            },
-            loading: () => const Center(child: CircularProgressIndicator()),
-          );
-        },
-      ),
+            loading: () => Container(
+              color: Colors.white,
+            ),
+            error: (error, stackTrace) => Center(child: Text('Error: $error')),
+          ),
+        ),
+        if (isLoading)
+          Container(
+            color: Colors.black54, // Semi-transparent background
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
     );
   }
 
-  Widget _buildDetailCard(String label, String value) {
-    return Card(
-      elevation: 0,
-      color: Colors.grey[50],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              // Use Expanded to prevent overflow when text is too long
-              child: Text(
-                label,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
-                  color: Colors.black87,
-                ),
-                overflow:
-                    TextOverflow.ellipsis, // Ensure the label does not overflow
-              ),
-            ),
-            Expanded(
-              // Use Expanded to prevent overflow for value
-              child: Text(
-                value,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black54,
-                ),
-                overflow:
-                    TextOverflow.ellipsis, // Ensure the value does not overflow
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  createRescheduledAppointment(
+      {required String date,
+      required String time,
+      required String appointmentStatus}) async {
+    setState(() {
+      isLoading =
+          true; // Set loading to true before starting the appointment creation
+    });
+    final supabase = Supabase.instance.client;
+
+    // Get the current time in UTC
+    DateTime timestamp = DateTime.now().toUtc();
+
+    final appointmentId = ref.watch(tappedSpAppointmentIdProvider);
+
+    final response = await supabase
+        .from('appointment')
+        .update({
+          'appointment_date': date,
+          'appointment_time': time,
+          'appointment_type': appointmentStatus,
+          'created_at': timestamp.toString(),
+        })
+        .eq('appointment_id', appointmentId)
+        .select('appointment_id')
+        .single();
+
+    final fetchedAppointmentId = response['appointment_id'];
+    print('Appointment created with ID: $appointmentId');
+    ref.read(appointmentIdProvider.notifier).state = appointmentId.toString();
+
+    setState(() {
+      isLoading =
+          false; // Set loading to false once the appointment creation is done
+    });
+
+    return fetchedAppointmentId;
   }
 }
